5437edc62979ee75c91fc6205b19c772a540ead7
Telath <clement.pouillart@gmail.com>
2023-02-26 19:27:24 +0100

    Changement du lien de mon cv pour l'ouvrir dans un nouvel onglet

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
                 <h1 class="title">Clément <span class="title">POUILLART</span></h1>
                 <p class="subtitle">Web développeur</p>
                 <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae lectus id nunc gravida maximus. Morbi congue urna vitae sem blandit lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus interdum enim vitae lacus aliquet, mattis dapibus eros facilisis. Sed ultrices metus eu blandit pulvinar</p>
-                <a href="https://firebasestorage.googleapis.com/v0/b/cp-project-2023.appspot.com/o/CV_CPouillart.pdf?alt=media&token=ec84413d-2dc5-4d90-b64d-adff906975b5">Mon CV</a>
+                <a href="https://firebasestorage.googleapis.com/v0/b/cp-project-2023.appspot.com/o/CV_CPouillart.pdf?alt=media&token=ec84413d-2dc5-4d90-b64d-adff906975b5" target="_blank" rel="noreferrer">Mon CV</a>
             </div>
             <div class="imgContent">
                 <img src="./9.svg" alt=""></img>
