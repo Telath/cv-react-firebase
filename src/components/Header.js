8bdf19feb2529d28c5fe4cb67cbbeaabea318893
Telath <clement.pouillart@gmail.com>
2023-02-26 19:24:31 +0100

    Ajout du lien de mon cv sur cloud storage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -8,7 +7,7 @@ const Header = () => {
                 <h1 class="title">Clément <span class="title">POUILLART</span></h1>
                 <p class="subtitle">Web développeur</p>
                 <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae lectus id nunc gravida maximus. Morbi congue urna vitae sem blandit lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus interdum enim vitae lacus aliquet, mattis dapibus eros facilisis. Sed ultrices metus eu blandit pulvinar</p>
-                <NavLink exact="true" to="/contact" class="contactBtn">Contact <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 12l11 3.1 7-8.1-8.156 5.672-4.312-1.202 15.362-7.68-3.974 14.57-3.75-3.339-2.17 2.925v-.769l-2-.56v7.383l4.473-6.031 4.527 4.031 6-22z"/></svg></span></NavLink>
+                <a href="https://firebasestorage.googleapis.com/v0/b/cp-project-2023.appspot.com/o/CV_CPouillart.pdf?alt=media&token=ec84413d-2dc5-4d90-b64d-adff906975b5">Mon CV</a>
             </div>
             <div class="imgContent">
                 <img src="./9.svg" alt=""></img>
